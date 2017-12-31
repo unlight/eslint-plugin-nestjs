463b5dccd822af5e8a6ba79a08fc54ba993f1497
roman <s91630277@gmail.com>
2017-12-31 13:08:20 +0400

    fix(deprecated-api-modules): Point to property where it defined

--- src/deprecated-api-modules/deprecated-api-modules.ts
+++ src/deprecated-api-modules/deprecated-api-modules.ts
@@ -3,20 +3,21 @@ export const deprecatedApiModules = {
     create(context) {
         return {
             ClassDeclaration: (node) => {
-                if (hasModulesProperty(node)) {
-                    context.report({ node, message });
+                const property = getModuleModulesProperty(node);
+                if (property) {
+                    context.report({ node: property, message });
                 }
             }
         };
     }
 };
 
-function hasModulesProperty(node) {
+function getModuleModulesProperty(node) {
     const decorator = (node.decorators || []).find(d => d.expression && d.expression.callee && d.expression.callee.name === 'Module');
     if (!decorator) {
         return false;
     }
     const [argument] = decorator.expression.arguments;
-    const result = (argument.type === 'ObjectExpression') && (argument.properties || []).some(property => property.key && property.key.name === 'modules');
+    const result = (argument.type === 'ObjectExpression') && (argument.properties || []).find(property => property.key && property.key.name === 'modules');
     return result;
 }
